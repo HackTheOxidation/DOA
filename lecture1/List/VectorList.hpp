4f9ed77ef4f0f3c444bc98eaa04ca885458fffeb
HackTheOxidation <tomas.hagenau@protonmail.ch>
2022-04-30 11:51:52 +0200

    Spring cleaning.

--- lecture1/List/VectorList.hpp
+++ lecture1/List/VectorList.hpp
@@ -7,55 +7,56 @@ using namespace std;
 
 template<typename T>
 class VectorList : public List<T> {
-	public:
-		VectorList<T>() : v() {
-			
-		}
+public:
+  VectorList<T>() : v() {}
+  void headInsert(const T& x) {
+    v.emplace(v.begin(), x);
+  }
 
-		void headInsert(const T& x) {
-			v.emplace(v.begin(), x);
-		}
+  void headRemove() {
+    v.erase(v.begin());
+  }
 
-		void headRemove() {
-			v.erase(v.begin());
-		}
+  unsigned int length() {
+    return v.size();
+  }
 
-		unsigned int length() {
-			return v.size();
-		}
+  void clear() {
+    if (length() == 0) {
+      return;
+    }
 
-		void clear() {
-			if (length() == 0) {
-				return;
-			}
+    v.clear();
+  }
 
-			v.clear();
-		}
+  T& at(unsigned int i) {
+    if (i >= length()) {
+      throw out_of_range("VectorList::at() : Index out of bounds");
+    }
+    
+    return v[i];
+  }
 
-		T& at(unsigned int i) {
-			if (i >= length()) {
-				throw out_of_range("VectorList::at() : Index out of bounds");
-			}
+  void remove(unsigned int index) {
+    if (index >= length()) {
+      throw out_of_range("VectorList::remove() : Index out of bounds");
+    }
+    
+    v.erase(v.begin() + index);
+  }
 
-			return v[i];
-		}
+  void insert(const T& x, unsigned int index) {
+    if (index >= length()) {
+      throw out_of_range("VectorList::insert() : Index out of bounds");	
+    }
+    
+    v.emplace(v.begin() + index);
+  }
 
-		void remove(unsigned int index) {
-			if (index >= length()) {
-				throw out_of_range("VectorList::remove() : Index out of bounds");
-			}
-			
-			v.erase(v.begin() + index);
-		}
+  T &operator[](unsigned int i) {
+    return this->at(i);
+  }
 
-		void insert(const T& x, unsigned int index) {
-			if (index >= length()) {
-				throw out_of_range("VectorList::insert() : Index out of bounds");	
-			}
-			
-			v.emplace(v.begin() + index);
-		}
-
-	private:
-		vector<T> v;
+private:
+  vector<T> v;
 };
